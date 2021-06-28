2247e9bf7f47297b7740c458f626b727f98f0cdd
sailesh130 <dedbond125@gmail.com>
2021-06-28 22:07:58 +0545

    spa-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem  } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     
 
@@ -22,12 +23,12 @@ function RenderDish({dish}){
             );
 }
 
-function RenderComments({dish}){
+function RenderComments({comments}){
 
-    if(dish != null)
+    if(comments != null)
     {
         
-       const com = dish.comments.map((comment)=>{
+       const com = comments.map((comment)=>{
             return(
             
                 
@@ -61,18 +62,26 @@ function RenderComments({dish}){
    
 const DishDetail = (props)=>{
 
-    return(
-
-            <div className="container">
+    return (
+                <div className="container">
                 <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish} />
-                  </div>
-                  <div className="col-md">
-                    <RenderComments dish={props.dish} />
+                    <Breadcrumb>
 
-                  </div>
-
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
                 </div>
                 </div>
             );
