59786a7982f1f670895ddde783a1e84438c552f6
sailesh130 <dedbond125@gmail.com>
2021-06-30 17:47:37 +0545

    action-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -24,8 +24,7 @@ class CommentForm extends Component{
 
     handleSubmit(values) {
         this.toggleModal();
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
         
 
     }
@@ -131,7 +130,7 @@ function RenderDish({dish}){
             );
 }
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, dishId}){
 
     if(comments != null)
     {
@@ -155,7 +154,7 @@ function RenderComments({comments}){
                   <ul className = "list-unstyled">
                         {com}
                   </ul> 
-                  <CommentForm />
+                  <CommentForm dishId={dishId} addComment={addComment} />
                 </div>   
             );
 
@@ -189,7 +188,9 @@ const DishDetail = (props)=>{
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments} 
+                         addComment={props.addComment}
+                        dishId={props.dish.id} />
                     </div>
                 </div>
                 </div>
